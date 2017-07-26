d6aa9f75018f56c661cbc0e7e99c91b2202e9ec9
David Chen <chenwei2000121@163.com>
2017-07-26 14:32:03 +0800

    Initial Commit

---
+++ CW File Viewer for Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CW File Viewer for Mac
+//
+//  Created by David Chen on 7/26/17.
+//  Copyright © 2017 David Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
